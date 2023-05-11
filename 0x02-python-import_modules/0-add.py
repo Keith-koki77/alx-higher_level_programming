a092738675072b5e353f989791b42fdb48616ae5
Keith koki <kokikeith@gmail.com>
2023-05-11 06:32:19 -0700

    first commit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# import the add function from add_0.py
+
+from add_0 import add
+
+# assign varaibles to a and b
+a = 1
+b = 2
+
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
