174a605b6107246a1b0f13afca04b7a53313777a
Keith koki <kokikeith@gmail.com>
2023-05-29 10:04:49 -0700

    mistaked fixed

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,8 +2,7 @@
 import sys
 def safe_print_integer_err(value):
     try:
-        integer_value = int(value)
-        print("{:d}".format(integer_value))
+        print("{:d}".format(value))
         return (True)
     except (ValueError, TypeError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
