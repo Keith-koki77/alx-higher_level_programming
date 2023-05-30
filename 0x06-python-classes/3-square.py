a93c1ec0a5c96814dfc617072564ee56c77d0e05
Keith koki <kokikeith@gmail.com>
2023-05-30 09:58:47 -0700

    file 3 committed

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Class square that defines asquare by:(based on: 2-square.py)"""
+
+class Square:
+
+    """Square class with it's size and proper validation"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+
+    def area(self):
+        return self.__size ** 2
