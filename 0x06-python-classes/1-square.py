21f16302de9bcb3cf65a5bbbcae88b7aee4caeca
Keith koki <kokikeith@gmail.com>
2023-05-30 05:28:36 -0700

    file 1 committed

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class square that defines a square by:(based on 0-square.py)"""
+class Square():
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """Initializing class square"""
+        self.__size = size
