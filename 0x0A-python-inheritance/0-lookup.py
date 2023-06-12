add76dbc4d143900dc8bb2886fc2cf172ba5f18d
Keith koki <kokikeith@gmail.com>
2023-06-12 13:31:16 -0700

    file update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,9 +3,5 @@
 """Returns the list of available attributes and methods"""
 
 def lookup(obj):
-    """Get the list of object attributes and methods"""
-    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
-    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
-
-    """Return the combined list of attributes and methods"""
-    return attributes + methods
+    """Returns a list of an object's available attributes"""
+    return(dir(obj))
