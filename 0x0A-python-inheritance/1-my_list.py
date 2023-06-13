4d5e532d199a9d9b7597187b6d4c9e73ec9dc4e9
Keith koki <kokikeith@gmail.com>
2023-06-12 18:59:53 -0700

    file 1 updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,5 +7,6 @@ class MyList(list):
     """class that inherits from list"""
     def print_sorted(self):
         """print sorted list"""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        res = list.copy(self)
+        list.sort(res)
+        print(res)
