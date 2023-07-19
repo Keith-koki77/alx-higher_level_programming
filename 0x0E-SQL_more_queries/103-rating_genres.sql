eddb70d89fc907a783408853d463911c29a77b53
Keith koki <kokikeith@gmail.com>
2023-07-19 13:22:46 -0700

    files updated

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT tv_genres.name, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN hbtn_0d_tvshows_rate ON tv_show_genres.show_id = hbtn_0d_tvshows_rate.show_id
+GROUP BY tv_genres.name
+ORDER BY rating_sum DESC;
