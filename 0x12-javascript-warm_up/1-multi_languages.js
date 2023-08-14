7d0dafdfa7b2a5ef882463fba0794285d62d6ed6
Keith koki <kokikeith@gmail.com>
2023-08-14 09:20:07 -0700

    file fixed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 
 // a script that prints 3 lines:
 
-console.log(line1);
-console.log(line2);
-console.log(line3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
