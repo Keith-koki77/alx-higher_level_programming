55964c33e511c4e8257ecb6875b21fd47d114ac7
Keith koki <kokikeith@gmail.com>
2023-08-15 23:47:20 -0700

    files committed

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const oldSquare = require('./5-square');
+
+class Square extends oldSquare {
+  charPrint (c) {
+    let k = c;
+    if (!c) {
+      k = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(k.repeat(this.width));
+    }
+  }
+}
+
+module.experts = Square;
