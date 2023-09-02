2ba0fd7caee80b3e1ad6bbd66e49ca19961278df
Keith koki <kokikeith@gmail.com>
2023-09-02 01:02:13 -0700

    file fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,9 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('- Server is: {}'.format(response.getheader('Server')))
-        print('- Date is: {}'.format(response.getheader('Date')))
-        print('- Content-Type is: {}'.format(response.getheader('Content-Type')))
-        print('- Content length is: {}'.format(len(html)))
-        print('- Message: {}'.format(html.decode('utf-8')))
+     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+         content = res.read()
+         print("Body Response:")
+         print("\t- type: {}".format(type(content)))
+         print("\t- content: {}".format(content))
+         print("\t- utf8 content: {}".format(content.decode('utf-8')))
