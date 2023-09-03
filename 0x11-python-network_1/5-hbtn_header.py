0ed8b070a7596ec526761084a207d2eb2a6fc050
Keith koki <kokikeith@gmail.com>
2023-09-03 00:24:53 -0700

    file fixed

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,22 +7,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-
-        x_request_id = response.headers.get('X-Request-Id')
-
-        if x_request_id:
-            print(f'X-Request-Id: {x_request_id}')
-        else:
-            print('X-Request-Id header not found in the response.')
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
