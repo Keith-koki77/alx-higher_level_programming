3c9bf2d7dd92ef8d0944d75e19d11d7758724ce2
Keith koki <kokikeith@gmail.com>
2023-09-03 00:35:42 -0700

    file fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,14 +7,15 @@ body of the response (decoded in utf-8)
 import sys
 import urllib.request
 import urllib.parse
+import urllib.error
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = parse.urlencode({'email': email}).encode('utf-8')
-    req = request.Request(url, data)
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data)
 
-    with request.urlopen(req) as response:
+    with urllib.request.urlopen(req) as response:
         html = response.read().decode('utf-8')
         print(html)
