a0eebbae6ba71db9d58b08a78848c5c182a6ea0f
Keith koki <kokikeith@gmail.com>
2023-09-03 00:54:02 -0700

    file fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,15 +9,18 @@ import requests
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
 
-    q = {'q': letter}
-    response = requests.post('http://0.0.0.0:5000/search_user', data=q)
+    params = {'q': letter}
+    response = requests.post('http://0.0.0.0:5000/search_user', params=params)
 
     json_response = response.json()
 
     if response.status_code == 200 and json_response:
-        for user in json_response:
-            print("["+str(user['id'])+"] "+user['name'])
+        if isinstance(json_response, list):
+            for user in json_response:
+                print(f"[{user.get('id')}] {user.get('name')}")
+        else:
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
     elif not json_response:
         print("No result")
     else:
-        print("Not a valid JSON")
+        print("No a valid JSON")
