1c8a36f078f17053908d3a884fdcad170913e973
Keith koki <kokikeith@gmail.com>
2023-09-05 02:53:22 -0700

    file fixed

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,21 +8,12 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database)
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (state, ))
-
-    rows = cursor.fetchall()
-
+    db_connect = db.connect(host="localhost", port=3306, user=argv[1],
+                            passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
+    db_cursor.execute("SELECT * FROM states \
+                        WHERE name LIKE BINARY %(name)s \
+                        ORDER BY states.id ASC", {'name': argv[4]})
+    rows = db_cursor.fetchall()
     for row in rows:
         print(row)
-
-db.close()
