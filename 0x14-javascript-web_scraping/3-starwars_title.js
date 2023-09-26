c81165f17226d96d462ff83931f2783c58a26924
Keith koki <kokikeith@gmail.com>
2023-09-26 06:58:51 -0700

    file fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,14 +2,27 @@
 // Script that prints the title of a Star Wars movie where the episode number matches a given integer
 
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/:id`;
-const id = process.argv[2];
 
-request.get(url, (error, response, body) => {
+// Check if a movie ID argument is provided
+if (process.argv.length < 3) {
+  console.error('Please provide a movie ID as the first argument.');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    console.log(data.title);
+    console.error('Error:', error);
+    return;
   }
+
+const data = JSON.parse(body);
+
+if (data && data.title) {
+  console.log(`${data.title}`);
+} else {
+  console.error('Movie not found or title not available.');
+}
 });
