e62fd1bf9ce8da38c5ec5ef1b0c570175a26ddb7
Keith koki <kokikeith@gmail.com>
2023-09-26 07:01:31 -0700

    file fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-// Script that prints the title of a Star Wars movie where the episode number matches a given integer
+// Script that prints the title of a Star Wars movie where
+// the episode number matches a given integer
 
 const request = require('request');
 
