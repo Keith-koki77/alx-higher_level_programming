4acf69547cb71cc3b1f7c49c7d27bc7a79d5551f
Keith koki <kokikeith@gmail.com>
2023-09-26 04:17:37 -0700

    first commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+//Script that reads and prints the content of a file.
+
+const fs = require('fs');
+
+// Get the file path from command line arguments
+const file = process.argv[2];
+
+// Read the file asynchronously with utf-8 encoding
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
